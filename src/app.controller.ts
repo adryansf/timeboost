e58acfa282c00b325eb382beab7cbad2ac3144d9
Adryan Freitas <adryansfreitas@gmail.com>
2024-08-03 18:10:30 -0300

    fix: Dockerfile

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -10,7 +10,7 @@ export class AppController {
     return this.appService.getHello();
   }
 
-  @Get()
+  @Get(':name')
   getHelloName(@Param('name') name: string): string {
     return this.appService.getHelloName(name);
   }
