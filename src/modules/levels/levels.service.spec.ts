1d9aa950675f724fc6a67001d1346e8f98645a33
majuzabbal <majuzabbal@gmail.com>
2024-08-24 16:05:18 -0300

    Test: levels

--- src/modules/levels/levels.service.spec.ts
+++ src/modules/levels/levels.service.spec.ts
@@ -1,19 +1,135 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { LevelsService } from './levels.service';
 import { LevelRepository } from './repository/level.repository';
+import { CreateLevelDto } from './dto/create-level.dto';
+import { UpdateLevelDto } from './dto/update-level.dto';
+import { NotFoundException } from '@nestjs/common';
+import { MESSAGES } from '@/utils/messages';
 
 describe('LevelsService', () => {
   let service: LevelsService;
+  let repository: LevelRepository;
+
+  const mockLevelRepository = {
+    create: jest.fn((dto) => {
+      return {
+        id: 1,
+        ...dto,
+      };
+    }),
+    findAll: jest.fn(() => [
+      { id: 1, name: 'Level 1', pointsRequired: 100 },
+      { id: 2, name: 'Level 2', pointsRequired: 200 },
+    ]),
+    findById: jest.fn((id) => {
+      if (id === 1) {
+        return Promise.resolve({ id: 1, name: 'Level 1', pointsRequired: 100 });
+      } else {
+        return Promise.resolve(null);
+      }
+    }),
+    update: jest.fn((id, dto) => ({ id, ...dto })),
+    delete: jest.fn((id) => ({ id, name: 'Deleted Level', pointsRequired: 0 })),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [LevelsService],
+      providers: [
+        LevelsService,
+        {
+          provide: LevelRepository,
+          useValue: mockLevelRepository,
+        },
+      ],
     }).compile();
 
     service = module.get<LevelsService>(LevelsService);
+    repository = module.get<LevelRepository>(LevelRepository);
   });
 
   it('should be defined', () => {
+    // Assert
     expect(service).toBeDefined();
+  });
+
+  it('should create a level', async () => {
+    // Arrange
+    const createLevelDto: CreateLevelDto = {
+      name: 'Level 1',
+      pointsRequired: 100,
+    };
+
+    // Act
+    const result = await service.create(createLevelDto);
+
+    // Assert
+    expect(result).toEqual({
+      id: expect.any(Number),
+      name: 'Level 1',
+      pointsRequired: 100,
+    });
+    expect(repository.create).toHaveBeenCalledWith(createLevelDto);
+  });
+
+  it('should return an array of levels', async () => {
+    // Act
+    const result = await service.findAll();
+
+    // Assert
+    expect(result).toEqual([
+      { id: 1, name: 'Level 1', pointsRequired: 100 },
+      { id: 2, name: 'Level 2', pointsRequired: 200 },
+    ]);
+  });
+
+  it('should update a level', async () => {
+    // Arrange
+    const updateLevelDto: UpdateLevelDto = {
+      name: 'Updated Level',
+      pointsRequired: 150,
+    };
+
+    // Act
+    const result = await service.update(1, updateLevelDto);
+
+    // Assert
+    expect(result).toEqual({
+      id: 1,
+      name: 'Updated Level',
+      pointsRequired: 150,
+    });
+  });
+
+  it('should throw NotFoundException if level not found during update', async () => {
+    // Arrange
+    const updateLevelDto: UpdateLevelDto = {
+      name: 'Updated Level',
+      pointsRequired: 150,
+    };
+
+    // Act & Assert
+    await expect(service.update(2, updateLevelDto)).rejects.toThrow(
+      new NotFoundException(MESSAGES.exception.level.NotFound),
+    );
+  });
+
+  it('should delete a level', async () => {
+    // Act
+    const result = await service.remove(1);
+
+    // Assert
+    expect(result).toEqual({
+      id: 1,
+      name: 'Deleted Level',
+      pointsRequired: 0,
+    });
+    expect(repository.delete).toHaveBeenCalledWith(1);
+  });
+
+  it('should throw NotFoundException if level not found during deletion', async () => {
+    // Act & Assert
+    await expect(service.remove(2)).rejects.toThrow(
+      new NotFoundException(MESSAGES.exception.level.NotFound),
+    );
   });
 });
---
+++ src/modules/levels/repository/level.repository.spec.ts
@@ -0,0 +1,147 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { PrismaService } from '@/database/prisma.service';
+import { LevelRepository } from './level.repository';
+
+describe('LevelRepository', () => {
+  let repository: LevelRepository;
+  let prismaService: PrismaService;
+
+  const mockPrismaService = {
+    level: {
+      create: jest.fn().mockResolvedValue({
+        id: 1,
+        name: 'Level 1',
+        pointsRequired: 100,
+      }),
+      findMany: jest.fn().mockResolvedValue([
+        { id: 1, name: 'Level 1', pointsRequired: 100 },
+        { id: 2, name: 'Level 2', pointsRequired: 200 },
+      ]),
+      findUnique: jest.fn().mockImplementation((params) => {
+        const { id } = params.where;
+        if (id === 1) {
+          return Promise.resolve({
+            id: 1,
+            name: 'Level 1',
+            pointsRequired: 100,
+          });
+        }
+        return Promise.resolve(null);
+      }),
+      update: jest.fn().mockResolvedValue({
+        id: 1,
+        name: 'Updated Level',
+        pointsRequired: 150,
+      }),
+      delete: jest.fn().mockResolvedValue({
+        id: 1,
+        name: 'Deleted Level',
+        pointsRequired: 0,
+      }),
+    },
+  };
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        LevelRepository,
+        {
+          provide: PrismaService,
+          useValue: mockPrismaService,
+        },
+      ],
+    }).compile();
+
+    repository = module.get<LevelRepository>(LevelRepository);
+    prismaService = module.get<PrismaService>(PrismaService);
+  });
+
+  it('should be defined', () => {
+    // Assert
+    expect(repository).toBeDefined();
+  });
+
+  it('should create a level', async () => {
+    // Arrange
+    const data = { name: 'Level 1', pointsRequired: 100 };
+
+    // Act
+    const result = await repository.create(data);
+
+    // Assert
+    expect(result).toEqual({
+      id: expect.any(Number),
+      name: 'Level 1',
+      pointsRequired: 100,
+    });
+    expect(prismaService.level.create).toHaveBeenCalledWith({ data });
+  });
+
+  it('should return an array of levels', async () => {
+    // Act
+    const result = await repository.findAll();
+
+    // Assert
+    expect(result).toEqual([
+      { id: 1, name: 'Level 1', pointsRequired: 100 },
+      { id: 2, name: 'Level 2', pointsRequired: 200 },
+    ]);
+    expect(prismaService.level.findMany).toHaveBeenCalled();
+  });
+
+  it('should find a level by id', async () => {
+    // Act
+    const result = await repository.findById(1);
+
+    // Assert
+    expect(result).toEqual({ id: 1, name: 'Level 1', pointsRequired: 100 });
+    expect(prismaService.level.findUnique).toHaveBeenCalledWith({
+      where: { id: 1 },
+    });
+  });
+
+  it('should return null if level is not found', async () => {
+    // Act
+    const result = await repository.findById(3);
+
+    // Assert
+    expect(result).toBeNull();
+    expect(prismaService.level.findUnique).toHaveBeenCalledWith({
+      where: { id: 3 },
+    });
+  });
+
+  it('should update a level', async () => {
+    // Arrange
+    const updateData = { name: 'Updated Level', pointsRequired: 150 };
+
+    // Act
+    const result = await repository.update(1, updateData);
+
+    // Assert
+    expect(result).toEqual({
+      id: 1,
+      name: 'Updated Level',
+      pointsRequired: 150,
+    });
+    expect(prismaService.level.update).toHaveBeenCalledWith({
+      where: { id: 1 },
+      data: updateData,
+    });
+  });
+
+  it('should delete a level', async () => {
+    // Act
+    const result = await repository.delete(1);
+
+    // Assert
+    expect(result).toEqual({
+      id: 1,
+      name: 'Deleted Level',
+      pointsRequired: 0,
+    });
+    expect(prismaService.level.delete).toHaveBeenCalledWith({
+      where: { id: 1 },
+    });
+  });
+});
